d6ccc58d2c3e68979845915d919d8d6066b0c2b2
Joseph Armstrong <70788681+armstjc@users.noreply.github.com>
2023-03-03 18:20:32 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "XFL Stats Fan Site",
-  tagline: "Because the actual XFL stats page is cringe.",
+  tagline: "Because the actual XFL stats page is terrible.",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
